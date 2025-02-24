ef66c3913a136324e1bbf4cced8be097653e704e
NaveeraRajChuhan <nnfstars@gmail.com>
2025-02-24 11:35:12 +0500

    trying console

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-var student = 'bareera';
-alert(student)
+// alert(popup msg)..............................................
+// var student = 'bareera';
+// alert(student)
+
+
+// alert(popup msg)..............................................
+// var a;
+// a ='hello'
+// alert (a);
+
+
+// console..............................................
+// var a;
+// console.log(a);
+// a='abcd'
+// console.log(a);
 
